6d6b55bf195be24f147e64a1a1cbc74844a08658
Edvardas Gaidelis <edvardassc@gmail.com>
2022-08-12 15:07:35 +0300

    add support for PHP 8.1

--- lib/Model/Omnimessage.php
+++ lib/Model/Omnimessage.php
@@ -393,7 +393,7 @@ class Omnimessage implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -418,7 +418,7 @@ class Omnimessage implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -434,7 +434,7 @@ class Omnimessage implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
--- lib/Model/SMS.php
+++ lib/Model/SMS.php
@@ -458,7 +458,7 @@ class SMS implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -483,7 +483,7 @@ class SMS implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -499,7 +499,7 @@ class SMS implements ModelInterface, ArrayAccess, \JsonSerializable
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
